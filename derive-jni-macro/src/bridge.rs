649f87f5908ef6e170cff25775409aaeffe1580e
fanchao <git@fanchao.dev>
2023-11-12 21:38:03 +1100

    JNI

--- derive-jni-macro/src/bridge.rs
+++ derive-jni-macro/src/bridge.rs
@@ -1,6 +1,8 @@
-use proc_macro2::TokenStream;
+use convert_case::{Case, Casing};
+use proc_macro2::{Ident, TokenStream};
+use proc_macro_error::abort;
 use quote::{format_ident, quote};
-use syn::{parse_quote, FnArg, ItemTrait, TraitItem, Type};
+use syn::{parse_quote, punctuated::Punctuated, Expr, FnArg, ItemTrait, Token, TraitItem, Type};
 
 pub fn make_jni_bridge(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let ItemTrait {
@@ -23,27 +25,48 @@ pub fn make_jni_bridge(_attr: TokenStream, item: TokenStream) -> TokenStream {
                 if f.default.is_none()
                     && matches!(f.sig.inputs.first(), Some(FnArg::Receiver(_))) =>
             {
-                // f.sig
-                //     .inputs
-                //     .insert(1, parse_quote! { env: &mut ::jni::JNIEnv<'_> });
-
-                let builder_code: Vec<TokenStream> = f
-                    .sig
+                f.sig
                     .inputs
-                    .iter()
-                    .skip(1)
-                    .map_while(|input| {
-                        let FnArg::Typed(input) = input else {
-                            return None;
-                        };
+                    .insert(1, parse_quote! { env: &mut ::jni::JNIEnv<'_> });
 
-                        let ty = &input.ty;
+                let mut builder_code: Vec<TokenStream> = vec![];
+                let mut call_list: Punctuated<Expr, Token![,]> = Default::default();
 
-                        Some(quote! {
-                            let builder = builder.add_argument::<#ty>();
-                        })
-                    })
-                    .collect();
+                for input in f.sig.inputs.iter().skip(2) {
+                    let FnArg::Typed(input) = input else {
+                        abort!(input, "Expected typed argument");
+                    };
+
+                    let (is_ref, ty) = match &*input.ty {
+                        Type::Path(path) => (false, &path.path),
+                        Type::Reference(reference) => match &*reference.elem {
+                            Type::Path(path) => (true, &path.path),
+                            _ => abort!(input, "Expected a type"),
+                        },
+                        _ => abort!(input, "Expected a type"),
+                    };
+
+                    builder_code.push(quote! {
+                         let builder = builder.add_argument::<#ty>();
+                    });
+
+                    let ident = match &*input.pat {
+                        syn::Pat::Ident(ident) => &ident.ident,
+                        _ => abort!(input, "Expected a name"),
+                    };
+
+                    let ident = if is_ref {
+                        quote! { #ident }
+                    } else {
+                        quote! { &#ident }
+                    };
+
+                    call_list.push(parse_quote! {
+                        <#ty as ::derive_jni::ToJavaValue>::into_java_value(#ident, env)?
+                    });
+                }
+
+                let java_method_name = f.sig.ident.to_string().to_case(Case::Camel);
 
                 let orig_output: Type;
 
@@ -67,8 +90,19 @@ pub fn make_jni_bridge(_attr: TokenStream, item: TokenStream) -> TokenStream {
                             builder.build::<#orig_output>()
                         };
 
-                        println!("sig: {sig}");
-                        todo!()
+                        let ret = env.call_method(
+                            self.get_java_object()?,
+                            #java_method_name,
+                            sig.as_str(),
+                            &[ #call_list ]
+                        )?;
+
+                        use ::derive_jni::ToRustType;
+
+                        match ret.to_rust_type(env) {
+                            Ok(v) => Ok(v),
+                            Err(e) => Err(::derive_jni::InvocationError::ReturnConvertError(Box::new(e))),
+                        }
                     }
                 });
             }
--- example/src/bin/derive.rs
+++ example/src/bin/derive.rs
@@ -1,11 +1,9 @@
-use std::net::IpAddr;
-
 use derive_jni::{java_class, WithJavaObject};
-use jni::objects::JObject;
+use jni::{objects::JObject, InitArgsBuilder, JavaVM};
 
 #[java_class]
 trait View {
-    fn set_text(&self, text: Option<i32>) -> &[i32];
+    fn hash_code(&self) -> i32;
 }
 
 struct MyView<'a>(JObject<'a>);
@@ -19,6 +17,20 @@ impl WithJavaObject for MyView<'_> {
 impl<'a> ViewJavaObject for MyView<'a> {}
 
 fn main() {
-    let view = MyView(JObject::null());
-    view.set_text(Some(2));
+    let vm = JavaVM::new(
+        InitArgsBuilder::default()
+            .build()
+            .expect("To build init args"),
+    )
+    .expect("To run java");
+
+    let mut guard = vm.attach_current_thread().expect("To attach thread");
+    let b = guard
+        .new_object("java/lang/Boolean", "(Z)V", &[true.into()])
+        .expect("To create boolean");
+
+    let code = MyView(b).hash_code(&mut guard).expect("To run hashcode");
+    println!("Got hashcode {code}");
+
+    // view.set_text(Some(2));
 }
